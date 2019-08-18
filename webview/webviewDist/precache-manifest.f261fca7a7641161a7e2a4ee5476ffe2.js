54fea25032e66ca72980862f872d2504d3e6b33f
aaaaash <binshao54@gmail.com>
2019-08-18 14:24:36 +0800

    eject and update webview build path.

---
+++ webview/webviewDist/precache-manifest.f261fca7a7641161a7e2a4ee5476ffe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db38b1d1f9e12627f865175bde63ef70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e92fec80511ac674c65d",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "49c8bac24bc14da5a2aa",
+    "url": "/static/js/2.81022a35.chunk.js"
+  },
+  {
+    "revision": "e92fec80511ac674c65d",
+    "url": "/static/js/main.e8d1e0b2.chunk.js"
+  },
+  {
+    "revision": "6f84714afee9a22dffd2",
+    "url": "/static/js/runtime~main.0b283736.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
