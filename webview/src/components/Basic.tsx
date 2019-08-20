40ad65ff2b00c1ba1341c8932de4bbafb1c7fb24
Aaaaash <binshao54@gmail.com>
2019-08-20 15:37:10 +0800

    Fix CI.

--- webview/src/components/Basic.tsx
+++ webview/src/components/Basic.tsx
@@ -63,8 +63,6 @@ const ExportedNames = () => {
             </p>
             <p>
                 When importing a package, you can refer only to its exported names.
-
-
                 Any "unexported" names are not accessible from outside the package.
             </p>
             <p>
